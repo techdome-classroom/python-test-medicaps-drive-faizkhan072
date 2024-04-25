1df143c9139d2a79b2beeb7bb93ff97e27eb78de
faizkhan072 <99603090+faizkhan072@users.noreply.github.com>
2024-04-25 14:07:56 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,18 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+     def romanToInt(self, s:str):
+        romeletter={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        sumrez=0
+        tempstr=list()
+        for letter in s:
+            for I,num1 in romanletter.items():
+                if letter==I:
+                    tempstr.append(int(num1))
+
+        for i in range(len(tempstr)-1):
+            if tempstr[i]<tempstr[i+1]:
+                tempstr[i]=-tempstr[i]
+
+        sumrez=sum(tempstr)
+        return sumrez
+        
         pass
